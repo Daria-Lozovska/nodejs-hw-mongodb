f4be830c934a7b34f6deef8425378db23f4a41fb
lozovskaadasa9@gmail.com <lozovskaadasa9@gmail.com>
2025-04-05 00:37:41 +0200

    correct mistakes and all done

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const initMongoConnection = async () => {
+  const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_URL, MONGODB_DB } = process.env;
+  const uri = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority&appName=Cluster0`;
+
   try {
-    await mongoose.connect("mongodb+srv://Daria_Lozovska:sl2SP2LuVIbmAKD0@cluster0.xcvyszj.mongodb.net/contacts?retryWrites=true&w=majority&appName=Cluster0");
+    await mongoose.connect(uri);
     console.log('Mongo connection successfully established!');
   } catch (error) {
     console.error('Mongo connection error:', error);
@@ -14,4 +14,3 @@ const initMongoConnection = async () => {
 };
 
 export default initMongoConnection;
-
