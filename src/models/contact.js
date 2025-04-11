4874144561709374643f25218f9c0d2171bfe91b
lozovskaadasa9@gmail.com <lozovskaadasa9@gmail.com>
2025-04-11 20:53:06 +0200

    all done and correct mistakes

--- src/models/contact.js
+++ src/models/contact.js
@@ -3,11 +3,11 @@ import mongoose from "mongoose";
 const contactSchema = new mongoose.Schema({
   name: { type: String, required: true },
   phoneNumber: { type: String, required: true },
-  email: { type: String },
-  isFavourite: { type: Boolean, default: false },
+  email: String,
+  isFavourite: Boolean,
   contactType: { type: String, required: true },
+}, {
+  timestamps: true 
 });
 
-const Contact = mongoose.model("Contact", contactSchema);
-
-export default Contact;
+export default contactSchema;
